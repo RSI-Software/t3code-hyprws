994d4e3106aa3e78d3f77ed0769d80e43d33ade6
donjor <donjordev@gmail.com>
2026-08-22 11:51:55 +1200

    feat(web): render scoped project shell
    Fork-Domain: project-windows
    Fork-Tier: core

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -153,9 +153,13 @@ function RootRouteView() {
 
   const appShell = (
     <CommandPalette>
-      <AppSidebarLayout>
+      {pathname.startsWith("/project/") ? (
         <Outlet />
-      </AppSidebarLayout>
+      ) : (
+        <AppSidebarLayout>
+          <Outlet />
+        </AppSidebarLayout>
+      )}
     </CommandPalette>
   );
 
