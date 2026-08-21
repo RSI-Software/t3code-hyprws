e1aa3e90e25d0f3ee34b684858bbab191ff0c757
donjor <donjordev@gmail.com>
2026-08-21 23:50:43 +0000

    feat(web): preserve project thread routes
    Fork-Domain: project-windows
    Fork-Tier: core

--- apps/web/src/hooks/useHandleNewThread.ts
+++ apps/web/src/hooks/useHandleNewThread.ts
@@ -32,7 +32,7 @@ import {
 import { readT3ProjectFile } from "../lib/t3ProjectFileDefaults";
 import { environmentServerConfigsAtom } from "../state/server";
 import { resolveThreadRouteTarget } from "../threadRoutes";
-import { resolveThreadRouteFamily } from "../lib/threadRouteNavigation";
+import { resolveThreadRouteFamily } from "../threadRoutes";
 import { legacyProjectCwdPreferenceKey, useUiStateStore } from "../uiStateStore";
 import { useClientSettings } from "./useSettings";
 
